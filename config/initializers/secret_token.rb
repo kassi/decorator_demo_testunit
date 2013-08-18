56822e07017b406c1955f972f08a67da2d226d94
Karsten Silkenbäumer <ksi@kluks.de>
2013-08-18 12:59:13 +0200

    Initial commit
    Command:
      rails new decorator_demo_testunit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DecoratorDemoTestunit::Application.config.secret_token = 'bb3bc56fa59a39809192f42d43d2a4238dfcca5f6dcdecdf50030c3626c741ccc7d9c5b607bb9569623dd4201e50e9b3bf1c0410a3a7049cc8ebc7c6a865a3f5'
